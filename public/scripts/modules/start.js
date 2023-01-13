3b043a3e7c212a5031b94c1340cdb9fe9685a682
Lucarta84 <lucarta@outlook.it>
2023-01-13 13:35:10 +0100

    Add right HTML AND JS template

---
+++ public/scripts/modules/start.js
@@ -0,0 +1,56 @@
+import { checkTime } from "./checkTime.js";
+//import { reset } from "./main.js";
+
+
+const times = document.querySelector('#times');
+let timeSplit = times.innerText.split(':')
+let ms = 0;
+let s = 0;
+let m = 0;
+let h = 0;
+console.log(ms, s, m, h)
+
+
+
+const reset = () => {
+  times.innerHTML = '00:00:00:00';
+  timeSplit = times.innerText.split(':');
+  ms = 0;
+  s = 0;
+  m = 0;
+  h = 0;
+  console.log(h, m, s, ms)
+}
+//const setMs=(value)=>{ms=value}
+
+const start = () => {
+  if (ms < 99) {
+    ms += 1;
+    timeSplit[3] = checkTime(ms);
+    document.querySelector('#times').innerHTML = timeSplit.join(':');
+  } else {
+    ms = 0;
+    timeSplit[3] = checkTime(ms);
+    if (s < 59) {
+      s += 1;
+      timeSplit[2] = checkTime(s);
+    } else {
+      s = 0;
+      timeSplit[2] = checkTime(s);
+      if (m < 59) {
+        m += 1;
+        timeSplit[1] = checkTime(m);
+      } else if (h < 59) {
+        m = 0;
+        timeSplit[1] = checkTime(m);
+        h += 1;
+        timeSplit[0] = checkTime(h);
+      }
+
+    }
+  }
+
+}
+
+
+export { start, ms, s, m, h, reset }
