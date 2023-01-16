32da76d9b0b069a86f664a8725da4f0b9e7518d6
Lucarta84 <lucarta@outlook.it>
2023-01-16 18:55:48 +0100

    If statament removed

--- public/scripts/modules/start.js
+++ public/scripts/modules/start.js
@@ -15,31 +15,17 @@ const reset = () => {
 //const setMs=(value)=>{ms=value}
 
 const start = () => {
-  if (timesDigit.milliseconds < 99) {
-    timesDigit.milliseconds += 1;
-    timeSplit[3] = String((timesDigit.milliseconds)).padStart(2, '0');
-    times.innerHTML = timeSplit.join(':');
-  } else {
-    timesDigit.milliseconds = 0;
-    timeSplit[3] = String((timesDigit.milliseconds)).padStart(2, '0');
-    if (timesDigit.seconds < 59) {
-      timesDigit.seconds += 1;
-      timeSplit[2] = String((timesDigit.seconds)).padStart(2, '0');
-    } else {
-      timesDigit.seconds = 0;
-      timeSplit[2] = String((timesDigit.seconds)).padStart(2, '0');
-      if (timesDigit.minute < 59) {
-        timesDigit.minute += 1;
-        timeSplit[1] = String((timesDigit.minute)).padStart(2, '0');
-      } else if (timesDigit.hours < 59) {
-        timesDigit.minute = 0;
-        timeSplit[1] = String((timesDigit.minute)).padStart(2, '0');
-        timesDigit.hours += 1;
-        timeSplit[0] = String((timesDigit.hours)).padStart(2, '0');
-      }
+  timesDigit.milliseconds += 1;
+  let timesDigitSeconds = parseInt(timesDigit.milliseconds / 100);
+  let timesDigitMinute = parseInt(timesDigit.milliseconds / 6000);
+  let timesDigitHour = parseInt(timesDigit.milliseconds / 360000);
 
-    }
-  }
+  timeSplit[3] = String(((timesDigit.milliseconds) % 99)).padStart(2, '0');
+  timeSplit[2] = String(((timesDigitSeconds) % 60)).padStart(2, '0');
+  timeSplit[1] = String(((timesDigitMinute) % 60)).padStart(2, '0');
+  timeSplit[0] = String(((timesDigitHour) % 99)).padStart(2, '0');
+
+  times.innerHTML = timeSplit.join(':');
 
 }
 
